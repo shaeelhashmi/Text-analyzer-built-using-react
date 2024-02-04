dc721987e8cf0dcd8c7df7b09a2850866f7d5ffd
shaeelhashmi <shaeelh001@gmail.com>
2024-02-04 23:30:44 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+let hello="My js variable"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{hello}</h1>
+      <nav>
+      <li>Home </li>
+      <li>Contact us</li>
+      <li>About us</li>
+      <li>Order</li>
+      <li>Deals</li></nav>
+      <h1>This is my first react app</h1>
+     
     </div>
   );
 }
